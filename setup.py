e3fe499ad5e3e59a04e2bfe0617bfeef1e5928f4
caspervdw <caspervdw@gmail.com>
2016-07-28 17:41:23 +0200

    Setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+import os
+from setuptools import setup
+
+try:
+    descr = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+except IOError:
+    descr = ''
+
+try:
+    from pypandoc import convert
+    descr = convert(descr, 'rst', format='md')
+except ImportError:
+    pass
+
+
+setup_parameters = dict(
+    name="clustertracking",
+    version='1.0',
+    description="An algorithm to track overlapping features in video images",
+    author="Casper van der Wel",
+    install_requires=['numpy', 'scipy', 'pandas', 'trackpy', 'pims'],
+    author_email="wel@physics.leidenuniv.nl",
+    url="https://github.com/caspervdw/clustertracking",
+    packages=['clustertracking',
+              'clustertracking.tests'],
+    long_description=descr)
+
+setup(**setup_parameters)
